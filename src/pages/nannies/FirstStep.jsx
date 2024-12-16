fbdbc594e8f47ecb40413d3cc19a20214f7c1bf3
giannisz2 <giannisz2001@gmail.com>
2024-12-16 13:14:40 +0200

    First step done

--- src/pages/nannies/FirstStep.jsx
+++ src/pages/nannies/FirstStep.jsx
@@ -4,12 +4,25 @@ import HelpButton from '../../components/buttons/HelpButton.jsx';
 import { Row, Col } from 'react-bootstrap';
 import { Select, MenuItem, TextField, InputLabel, FormControl } from '@mui/material';
 import '../../styles/FirstStep.css'
+import Datepicker from '../../components/layout/Datepicker.jsx';
 
 export default function FirstStep() {
     return (
         <div className="profile-edit-nannies d-flex flex-column min-vh-100">
             <HelpButton />
             <Logo />
+            <div class="stepper">
+                <div class="step active">
+                    <div class="circle">1</div>
+                    <div class="label">ΣΤΟΙΧΕΙΑ</div>
+                </div>
+                <div class="step">
+                    <div class="circle">2</div>
+                </div>
+                <div class="step">
+                    <div class="circle">3</div>
+                </div>
+            </div>
             <div className="content flex-grow-1 d-flex align-items-center justify-content-center">
                 <Row className="align-items-start justify-content-center g-5 m-0 w-100">
                     <Col
@@ -37,7 +50,13 @@ export default function FirstStep() {
                             type="text"
                             className="my-3"
                         />
-                        <TextField fullWidth label="Είστε καπνιστής;" type="text" className="my-3" />
+                        <FormControl fullWidth className="my-3">
+                            <InputLabel>Είστε καπνιστής;</InputLabel>
+                            <Select defaultValue="">
+                                <MenuItem value="ΝΑΙ">ΝΑΙ</MenuItem>
+                                <MenuItem value="ΟΧΙ">ΟΧΙ</MenuItem>
+                            </Select>
+                        </FormControl>
                     </Col>
                     <Col
                         md={6}
@@ -45,17 +64,19 @@ export default function FirstStep() {
                         className="d-flex flex-column align-items-center justify-content-center text-center"
                     >
                         <TextField fullWidth label="Επώνυμο" type="text" className="my-3" />
-                        <TextField fullWidth label="Ημερομηνία γέννησης" type="text" className="my-3" />
+                        <Datepicker/>
                         <TextField fullWidth label="Διαθεσιμότητα" type="text" className="my-3" />
                         <TextField fullWidth label="Εμπειρία" type="text" className="my-3" />
                         <TextField fullWidth label="Λίγα λόγια για εσάς" type="text" className="my-3" />
                         <TextField fullWidth label="Τοποθεσία" type="text" className="my-3" />
-                        <TextField
-                            fullWidth
-                            label="Είστε διατεθειμένος να εργαστείτε σε σπίτι με κατοικίδια;"
-                            type="text"
-                            className="my-3"
-                        />
+                        <FormControl fullWidth className="my-3">
+                            <InputLabel>Είστε διαθέσιμος να εργαστείτε σε σπίτι με κατοικίδια;</InputLabel>
+                            <Select defaultValue="">
+                                <MenuItem value="ΝΑΙ">ΝΑΙ</MenuItem>
+                                <MenuItem value="ΟΧΙ">ΟΧΙ</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <button id="button-apply">Οριστική Υποβολή</button>
                     </Col>
                 </Row>
             </div>
