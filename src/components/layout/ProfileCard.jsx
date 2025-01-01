7286697161d2e17323d88d5f48b52bc783a52284
DimitraPoliti <sdi2200151@di.uoa.gr>
2025-01-01 16:23:34 +0200

    searchnannies leipei topothesia

--- src/components/layout/ProfileCard.jsx
+++ src/components/layout/ProfileCard.jsx
@@ -4,17 +4,19 @@ import "../../styles/ProfileCard.css";
 export default function ProfileCard({ nanny }) {
     return (
         <div className="profile-card">
-            <div className="profile-image-placeholder">
-                <img src="/path/to/placeholder.png" alt={nanny.name} />
-            </div>
             <div className="profile-details">
-                <h3>{nanny.name}</h3>
-                <p><strong>Ηλικία:</strong> {nanny.age}</p>
-                <p><strong>Ειδίκευση:</strong> {nanny.specialization}</p>
-                <p><strong>Εμπειρία:</strong> {nanny.experience}</p>
-                <p><strong>Σπουδές:</strong> {nanny.studies}</p>
-                <p><strong>Χρόνος Απασχόλησης:</strong> {nanny.employmentTime === 'full-time' ? 'Πλήρης Απασχόληση' : 'Μερική Απασχόληση'}</p>
-                <p>{nanny.description}</p>
+                <div className="profile-image-placeholder">
+                    <img src="/path/to/placeholder.png" alt={nanny.name} />
+                </div>
+                <div className="profile-info">
+                    <h3>{nanny.name}</h3>
+                    <p><strong>Ηλικία:</strong> {nanny.age}</p>
+                    <p><strong>Ειδίκευση:</strong> {nanny.specialization}</p>
+                    <p><strong>Εμπειρία:</strong> {nanny.experience}</p>
+                    <p><strong>Σπουδές:</strong> {nanny.studies}</p>
+                    <p><strong>Χρόνος Απασχόλησης:</strong> {nanny.employmentTime === 'full-time' ? 'Πλήρης Απασχόληση' : 'Μερική Απασχόληση'}</p>
+                    <p>{nanny.description}</p>
+                </div>
             </div>
         </div>
     );
